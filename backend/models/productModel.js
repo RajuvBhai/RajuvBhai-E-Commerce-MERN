a7f44bc7c04acebad01f99fc500b69fd7df11f9f
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-13 18:52:38 +0530

    Create product modal

---
+++ backend/models/productModel.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true, "Please enter product name"],
+        trim: true,
+        maxLength: [100, "product name can not exceed 100 character"]
+    },
+    price: {
+        type: Number,
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter product description"]
+    },
+    ratings: {
+        type: String,
+        default: 0
+    },
+    images: [
+        {
+            fileName: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, "Please enter product category"],
+        enum: {
+            values: [
+                'Electronics',
+                'Mobile Phones',
+                'Laptops',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home',
+            ],
+            message: "Please select correct category"
+        }
+    },
+    seller: {
+        type: String,
+        required: [true, "Please enter product seller"]
+    },
+    stock: {
+        type: Number,
+        required: [true, "Please enter product stock"],
+        maxLength: [20, "Product stock cannot exceed 20"]
+    },
+    numOfReviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type:String,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+let schema = mongoose.model('Product', productSchema);
+
+module.exports = schema;
