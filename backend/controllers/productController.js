b21887a28130056227802f232b35896b2c245a78
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-13 23:18:09 +0530

    Add Get single product functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const productModel = require('../models/productModel');
 
 // Get All Products - /api/v1/products
-exports.getProducts = async (req, res, next)=>{
+exports.getAllProducts = async (req, res, next)=>{
     const products = await productModel.find();
     res.status(200).json({
         success: true,
@@ -13,6 +13,24 @@ exports.getProducts = async (req, res, next)=>{
 // Create Product - /api/v1/product/new
 exports.newProduct = async (req, res, next) => {
     const product = await productModel.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
+// Get single Products - /api/v1/products
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await productModel.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
     res.status(201).json({
         success: true,
         product
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getProducts, newProduct } = require('../controllers/productController');
+const { getAllProducts, newProduct, getSingleProduct } = require('../controllers/productController');
 const router = express.Router();
 
-router.route('/products').get(getProducts);
+router.route('/products').get(getAllProducts);
 router.route('/product/new').post(newProduct);
+router.route('/product/:id').get(getSingleProduct);
 
 module.exports = router;
