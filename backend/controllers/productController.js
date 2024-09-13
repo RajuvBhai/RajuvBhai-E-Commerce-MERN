6ab8209c3545c6fcf39fadde13177b841210fe03
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-13 22:19:57 +0530

    Add product create handler functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,18 @@
+const productModel = require('../models/productModel');
+
+
 exports.getProducts = (req, res, next)=>{
     res.status(200).json({
         success: true,
         message: "This route will show all the products"
+    })
+}
+
+// Create Product - /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+    const product = await productModel.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
     })
 }
