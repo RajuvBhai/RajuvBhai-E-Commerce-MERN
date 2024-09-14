92858cb824d436a68f6ff1d437525d6c4b17f5b5
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-14 19:55:07 +0530

    Handle unhandling error -->{Server (unhandledRejection)}

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,9 +6,6 @@ const connectDatabase = () => {
         useUnifiedTopology: true
     }).then(con => {
         console.log(`MongoDb is connected to the host: ${con.connection.host}`);        
-    }).catch((err)=> {
-        console.log(err);
-        
     })
 }
 
