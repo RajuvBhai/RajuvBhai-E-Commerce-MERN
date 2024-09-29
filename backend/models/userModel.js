b0cfe0239b8302fd58e24cdf5eb8e2cc4ce29533
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-29 18:10:32 +0530

    Generate Reset password Token with i crypto

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -50,6 +51,17 @@ userSchema.methods.getJwtToken = function(){
 
  userSchema.methods.isValidPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password)
+ }
+
+ userSchema.methods.resetPassword = function(){
+    // Generate Token 
+    const token = crypto.randomBytes(20).toString('hex');
+
+    // Generate Hash and set to resetPasswordToken 
+    this.resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    // set token expire time 
+    this.resetPasswordTokenExpire = Date.now() + 30 * 60 * 1000;
  }
 
 let schema = mongoose.model('User', userSchema);
