bf437e2ce0fab67ad4c28fd724e790a4c8e4f8bf
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-10-01 22:18:44 +0530

    Error Handling User api services

--- backend/midlewares/error.js
+++ backend/midlewares/error.js
@@ -23,7 +23,22 @@ module.exports = (err, req, res, next) => {
             message = `Resource not found: ${err.path}` ;
             error = new Error(message);
         }
+
+        if(err.code == 11000) {
+            let message = `Duplicate ${Object.keys(err.keyValue)} error`;
+            error = new Error(message);
+        }
+
+        if(err.name == 'JSONWebTokenError') {
+            let message = `JSON Web Token is invalid. Try agin`;
+            error = new Error(message);
+        }
         
+        if(err.name == 'TokenExpiredError') {
+            let message = `JSON Web Token is expired. Try agin`;
+            error = new Error(message);
+        }
+
         res.status(err.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error',
